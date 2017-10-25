dfe45ddf73804330727d751c5397e92c25e23690
Matthew Morgan <matt@dulcetsoftware.com>
2017-10-25 07:11:09 -0400

    Get UP working

--- spec/round.spec.js
+++ spec/round.spec.js
@@ -1,14 +1,14 @@
 import {round, simpleRound, DIRECTIONS} from '../src/round';
 
-describe('Round HALF_UP for positive numbers', ()=> {
-  test('can round pi HALF_UP to 2 decimal places', ()=>{
+describe('Round HALF_UP for positive numbers', () => {
+  test('can round pi HALF_UP to 2 decimal places', () => {
     let number = 3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 2;
     expect(round({number, direction, precision})).toEqual(3.14);
   });
 
-  test('can round pi HALF_UP to 3 decimal places', ()=>{
+  test('can round pi HALF_UP to 3 decimal places', () => {
     let number = 3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 3;
@@ -16,15 +16,15 @@ describe('Round HALF_UP for positive numbers', ()=> {
   });
 });
 
-describe('Round HALF_UP for negative numbers', ()=> {
-  test('can round -pi HALF_UP to 2 decimal places', ()=>{
+describe('Round HALF_UP for negative numbers', () => {
+  test('can round -pi HALF_UP to 2 decimal places', () => {
     let number = -3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 2;
     expect(round({number, direction, precision})).toEqual(-3.14);
   });
 
-  test('can round -pi HALF_UP to 3 decimal places', ()=>{
+  test('can round -pi HALF_UP to 3 decimal places', () => {
     let number = -3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 3;
@@ -32,36 +32,36 @@ describe('Round HALF_UP for negative numbers', ()=> {
   });
 });
 
-describe('Round HALF_UP handles edge cases', ()=> {
-  test('supplied precision is longer than number', ()=> {
+describe('Round HALF_UP handles edge cases', () => {
+  test('supplied precision is longer than number', () => {
     let number = 3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 10;
     expect(round({number, direction, precision})).toEqual(3.14159);
   });
 
-  test('number is undefined', ()=> {
+  test('number is undefined', () => {
     let number;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 10;
     expect(round({number, direction, precision})).toEqual(0);
   });
 
-  test('direction is out of range', ()=> {
+  test('direction is out of range', () => {
     let number = 3.14159;
     let direction = 666;
     let precision = 3;
     expect(round({number, direction, precision})).toEqual(3.142);
   });
 
-  test('can correctly round a four', ()=> {
+  test('can correctly round a four', () => {
     let number = 3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 1;
     expect(round({number, direction, precision})).toEqual(3.1);
   });
 
-  test('can correctly round a four when negative', ()=> {
+  test('can correctly round a four when negative', () => {
     let number = -3.14159;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 1;
@@ -69,14 +69,14 @@ describe('Round HALF_UP handles edge cases', ()=> {
   });
 
 
-  test('can correctly round a six', ()=> {
+  test('can correctly round a six', () => {
     let number = 1.23456;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 4;
     expect(round({number, direction, precision})).toEqual(1.2346);
   });
 
-  test('can correctly round a six when negative', ()=> {
+  test('can correctly round a six when negative', () => {
     let number = -1.23456;
     let direction = DIRECTIONS.HALF_UP;
     let precision = 4;
@@ -146,112 +146,172 @@ describe('Round HALF_UP handles edge cases', ()=> {
 //   });
 // });
 //
-// describe('Round UP for positive numbers', ()=> {
-//   test('can round pi UP to 2 decimal places', ()=>{
-//     let number = 3.14159;
-//     let direction = DIRECTIONS.UP;
-//     let precision = 2;
-//     expect(round({number, direction, precision})).toEqual(3.15);
-//   });
-//
-//   test('can round pi UP to 3 decimal places', ()=>{
-//     let number = 3.14159;
-//     let direction = DIRECTIONS.UP;
-//     let precision = 3;
-//     expect(round({number, direction, precision})).toEqual(3.142);
-//   });
-// });
-//
-// describe('Round UP for negative numbers', ()=> {
-//   test('can round -pi UP to 2 decimal places', ()=>{
-//     let number = -3.14159;
-//     let direction = DIRECTIONS.UP;
-//     let precision = 2;
-//     expect(round({number, direction, precision})).toEqual(-3.15);
-//   });
-//
-//   test('can round -pi UP to 3 decimal places', ()=>{
-//     let number = -3.14159;
-//     let direction = DIRECTIONS.UP;
-//     let precision = 3;
-//     expect(round({number, direction, precision})).toEqual(-3.142);
-//   });
-// });
-//
-// describe('Round UP handles edge cases', ()=> {
-//   test('supplied precision is longer than number', ()=> {
-//     let number = 3.14159;
-//     let direction = DIRECTIONS.UP;
-//     let precision = 10;
-//     expect(round({number, direction, precision})).toEqual(3.14159);
-//   });
-//
-//   test('number is undefined', ()=> {
-//     let number;
-//     let direction = DIRECTIONS.UP;
-//     let precision = 10;
-//     expect(round({number, direction, precision})).toEqual(0);
-//   });
-// });
-//
-describe('simpleRound gives the same results as HALF_UP', ()=> {
-  test('simpleRound can round pi HALF_UP to 2 decimal places', ()=>{
+describe('Round UP for positive numbers', () => {
+  test('can round pi UP to 2 decimal places', () => {
+    let number = 3.14159;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(3.15);
+  });
+
+  test('can round pi UP to 3 decimal places', () => {
+    let number = 3.14159;
+    let direction = DIRECTIONS.UP;
+    let precision = 3;
+    expect(round({number, direction, precision})).toEqual(3.142);
+  });
+});
+
+describe('Round UP for negative numbers', () => {
+  test('can round -pi UP to 2 decimal places', () => {
+    let number = -3.14159;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(-3.15);
+  });
+
+  test('can round -pi UP to 3 decimal places', () => {
+    let number = -3.14159;
+    let direction = DIRECTIONS.UP;
+    let precision = 3;
+    expect(round({number, direction, precision})).toEqual(-3.142);
+  });
+});
+
+describe('round UP handles boundaries', () => {
+  test('rounds 5', () => {
+    let number = 0.285;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(0.29);
+  });
+
+  test('rounds 5 when negative', () => {
+    let number = -0.285;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(-0.29);
+  });
+
+  test('rounds 9', () => {
+    let number = 0.299;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(0.30);
+  });
+
+  test('rounds 9 when negative', () => {
+    let number = -0.299;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(-0.30);
+  });
+
+  test('doesnt round when precision is the same as the length of the number', () => {
+    let number = 0.999;
+    let direction = DIRECTIONS.UP;
+    let precision = 3;
+    expect(round({number, direction, precision})).toEqual(0.999);
+  });
+
+  test('doesnt round when precision is the same as the length of the negative number', () => {
+    let number = -0.999;
+    let direction = DIRECTIONS.UP;
+    let precision = 3;
+    expect(round({number, direction, precision})).toEqual(-0.999);
+  });
+
+
+  test('rounds a long number', () => {
+    let number = 0.99999999;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(1.00);
+  });
+
+  test('rounds a long negative number', () => {
+    let number = -0.99999999;
+    let direction = DIRECTIONS.UP;
+    let precision = 2;
+    expect(round({number, direction, precision})).toEqual(-1.00);
+  });
+});
+
+
+describe('Round UP handles edge cases', () => {
+  test('supplied precision is longer than number', () => {
+    let number = 3.14159;
+    let direction = DIRECTIONS.UP;
+    let precision = 10;
+    expect(round({number, direction, precision})).toEqual(3.14159);
+  });
+
+  test('number is undefined', () => {
+    let number;
+    let direction = DIRECTIONS.UP;
+    let precision = 10;
+    expect(round({number, direction, precision})).toEqual(0);
+  });
+});
+
+describe('simpleRound gives the same results as HALF_UP', () => {
+  test('simpleRound can round pi HALF_UP to 2 decimal places', () => {
     let number = 3.14159;
     expect(simpleRound(number)).toEqual(3.14);
   });
 
-  test('simpleRound can round -pi HALF_UP to 2 decimal places', ()=>{
+  test('simpleRound can round -pi HALF_UP to 2 decimal places', () => {
     let number = -3.14159;
     expect(simpleRound(number)).toEqual(-3.14);
   });
 
-  test('number is undefined', ()=> {
+  test('number is undefined', () => {
     let number;
     expect(simpleRound(number)).toEqual(0);
   });
 });
 
-describe('simpleRound works for decimal fractions less than one', ()=> {
-    test('a small number', () => {
-        let number = 0.285;
-        expect(simpleRound(number)).toEqual(0.29);
-    });
+describe('simpleRound works for decimal fractions less than one', () => {
+  test('a small number', () => {
+    let number = 0.285;
+    expect(simpleRound(number)).toEqual(0.29);
+  });
 
-    test('a smaller number', () => {
-        let number = 0.284;
-        expect(simpleRound(number)).toEqual(0.28);
-    });
+  test('a smaller number', () => {
+    let number = 0.284;
+    expect(simpleRound(number)).toEqual(0.28);
+  });
 
-    test('a number that causes rollover', () => {
-        let number = 0.099;
-        expect(simpleRound(number)).toEqual(.10);
-    });
+  test('a number that causes rollover', () => {
+    let number = 0.099;
+    expect(simpleRound(number)).toEqual(.10);
+  });
 
-    test('a number that causes rollover', ()=> {
-        let number = 0.999;
-        expect(simpleRound(number)).toEqual(1.00);
-    });
+  test('a number that causes rollover', () => {
+    let number = 0.999;
+    expect(simpleRound(number)).toEqual(1.00);
+  });
 
-    test('a number that causes rollover', ()=> {
-        let number = 0.99;
-        expect(simpleRound(number)).toEqual(0.99);
-    });
+  test('a number that causes rollover', () => {
+    let number = 0.99;
+    expect(simpleRound(number)).toEqual(0.99);
+  });
 
 
-    test('handles non-terminating decimals that round down', ()=> {
-      let number = 0.333333;
-      expect(simpleRound(number)).toEqual(0.33);
-    });
+  test('handles non-terminating decimals that round down', () => {
+    let number = 0.333333;
+    expect(simpleRound(number)).toEqual(0.33);
+  });
 
-    test('handles non-terminating decimals that round up', ()=> {
-        let number = 0.6666666;
-        expect(simpleRound(number)).toEqual(0.67);
-    });
+  test('handles non-terminating decimals that round up', () => {
+    let number = 0.6666666;
+    expect(simpleRound(number)).toEqual(0.67);
+  });
 
-    test('handles non-terminating decimals that round up', ()=> {
-        let number = 0.09666;
-        expect(simpleRound(number)).toEqual(0.10);
-    });
+  test('handles non-terminating decimals that round up', () => {
+    let number = 0.09666;
+    expect(simpleRound(number)).toEqual(0.10);
+  });
 });
 
 
--- src/round.js
+++ src/round.js
@@ -1,86 +1,91 @@
 const DIRECTIONS = {
-    HALF_UP: -1,
-    HALF_DOWN: 0,
-    UP: 1,
-    DOWN: 2
+  HALF_UP:   -1,
+  HALF_DOWN: 0,
+  UP:        1,
+  DOWN:      2
 };
 
 function simpleRound(number = 0) {
-    return round({number});
+  return round({number});
 }
 
 // https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 if (!String.prototype.padStart) {
-    String.prototype.padStart = function padStart(targetLength,padString) {
-        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
-        padString = String(padString || ' ');
-        if (this.length > targetLength) {
-            return String(this);
-        }
-        else {
-            targetLength = targetLength-this.length;
-            if (targetLength > padString.length) {
-                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
-            }
-            return padString.slice(0,targetLength) + String(this);
-        }
-    };
+  String.prototype.padStart = function padStart(targetLength, padString) {
+    targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+    padString = String(padString || ' ');
+    if (this.length > targetLength) {
+      return String(this);
+    }
+    else {
+      targetLength = targetLength - this.length;
+      if (targetLength > padString.length) {
+        padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+      }
+      return padString.slice(0, targetLength) + String(this);
+    }
+  };
 }
 
 function round({number = 0, direction = DIRECTIONS.HALF_UP, precision = 2}) {
 
-    let numberString = String(number);
-    numberString = numberString.indexOf('.') !== -1 ? numberString : `${numberString}.`;
-    const [originalInteger, fullDecimal] = numberString.split('.');
-    if (fullDecimal.length < precision +1){
-        return number;
-    }
+  let numberString = String(number);
+  numberString = numberString.indexOf('.') !== -1 ? numberString : `${numberString}.`;
+  const [originalInteger, fullDecimal] = numberString.split('.');
+  if (fullDecimal.length < precision + 1) {
+    return number;
+  }
 
-    const unroundedDecimal = fullDecimal.slice(0, precision+1);
-    let leadingZeroes = countLeadingZeroes(unroundedDecimal);
-    let roundingHint = unroundedDecimal.slice(-1);
-    let decimalPortion = unroundedDecimal.slice(0, precision);
+  const unroundedDecimal = fullDecimal.slice(0, precision + 1);
+  let leadingZeroes = countLeadingZeroes(unroundedDecimal);
+  let roundingHint = unroundedDecimal.slice(-1);
+  let decimalPortion = unroundedDecimal.slice(0, precision);
 
-    switch (direction) {
-        default:
-        case DIRECTIONS.HALF_UP: {
-            let integerPortion;
-            if (+roundingHint >= 5) {
-                let decimalNumber = Number(decimalPortion) + 1;
-                if (decimalNumber - decimalNumber % 10 > decimalPortion - decimalPortion % 10){
-                    let decStart = 0;
-                    let integerNumber;
-                    if (leadingZeroes === 0){
-                        integerNumber = Number(originalInteger) + 1;
-                        decStart = 1;
-                    } else {
-                        integerNumber = Number(originalInteger);
-                        leadingZeroes--;
-                    }
-                    decimalPortion = String(decimalNumber).slice(decStart, precision).padStart(leadingZeroes, '0');
-                    integerPortion = String(integerNumber);
-                } else {
-                    decimalPortion = String(decimalNumber);
-                    integerPortion = String(originalInteger);
-                }
-            } else {
-                integerPortion = String(originalInteger);
-            }
-            return Number(`${integerPortion}.${decimalPortion}`);
+  switch (direction) {
+    default:
+    case DIRECTIONS.HALF_UP: {
+      let integerPortion;
+      if (+roundingHint >= 5) {
+        let decimalNumber = Number(decimalPortion) + 1;
+        if (decimalNumber - decimalNumber % 10 > decimalPortion - decimalPortion % 10) {
+          let decStart = 0;
+          let integerNumber;
+          if (leadingZeroes === 0) {
+            integerNumber = Number(originalInteger) + 1;
+            decStart = 1;
+          } else {
+            integerNumber = Number(originalInteger);
+            leadingZeroes--;
+          }
+          decimalPortion = String(decimalNumber).slice(decStart, precision).padStart(leadingZeroes, '0');
+          integerPortion = String(integerNumber);
+        } else {
+          decimalPortion = String(decimalNumber);
+          integerPortion = String(originalInteger);
         }
+      } else {
+        integerPortion = String(originalInteger);
+      }
+      return Number(`${integerPortion}.${decimalPortion}`);
     }
+    case DIRECTIONS.UP: {
+      let power = 10 ** precision;
+      let sign = Math.sign(number);
+      return Math.ceil(Math.abs(number) * power) / power * sign;
+    }
+  }
 }
 
-function countLeadingZeroes(num){
-    let i = 0;
-    for (let n of num){
-        if (n !== '0'){
-            return i;
-        }
-        i++;
+function countLeadingZeroes(num) {
+  let i = 0;
+  for (let n of num) {
+    if (n !== '0') {
+      return i;
     }
-    return i;
+    i++;
+  }
+  return i;
 }
 
 export {round, simpleRound, DIRECTIONS};
